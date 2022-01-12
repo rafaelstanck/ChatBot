116602a0c96adbdca1b020f0d39c5b64da20f57e
Rafael Stanck <86021032+rafaelstanck@users.noreply.github.com>
2022-01-12 16:31:40 -0300

    Acrescentando mais linhas no programa

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ for i in pedido:
     if i in pergunta:
         resposta = i
 
+for i in informacoes:
+    if i in pergunta:
+        resposta = i
+
 if resposta in pedido:
     print('Ok, vamos ao que interessa!')
     pergunta = input(str('Gostaria de ver nosso cardápio? ')).lower()
@@ -26,7 +30,26 @@ if resposta in pedido:
 
     for i in cardapio:
         if i in pergunta:
-            print('Vamos ao cardápio:')
+            print('Claro, vamos ao cardápio!!!')
+            print(30 * '-')
+            print('LANCHES')
+            print(30 * '-')
+            print('1. Misto Quente R$\n'
+                  '2. Queijo Quente R$\n'
+                  '3. Bauru R$\n'
+                  '4. X-Burguer R$\n'
+                  '5. X-Salada R$\n'
+                  '6. X-Egg R$\n'
+                  '7. X-Calabresa R$\n'
+                  '8. X-Bacon R$\n'
+                  '9. X-Coração R$\n'
+                  '10. X-Tudo R$\n')
+            print(30* '-')
+            print('PORÇÕES')
+            print(30 * '-')
+
+elif resposta in informacoes:
+    print('Gostaria de saber qual o nosso horário de atendimento? ')
 
 else:
     print('Eu não entendi o que voce quis dizer. Vamos tentar novamente?')
